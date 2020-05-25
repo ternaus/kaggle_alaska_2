a847a86ea11d47bd486baabb44ffca442b230f05
Vladimir Iglovikov <iglovikov@gmail.com>
2020-05-25 14:40:10 -0700

    Added logger

--- kaggle_alaska_2/dataloader.py
+++ kaggle_alaska_2/dataloader.py
@@ -1,29 +1,50 @@
+import random
 from pathlib import Path
 from typing import List, Tuple, Dict, Any
 
 import albumentations as albu
+import torch
 from iglovikov_helper_functions.utils.image_utils import load_rgb
 from pytorch_toolbelt.utils.torch_utils import tensor_from_rgb_image
 from torch.utils.data import Dataset
+import cv2
 
 
 class Alaska2Dataset(Dataset):
-    def __init__(self, samples: List[Tuple[Path, Path]], transform: albu.Compose) -> None:
+    def __init__(self, samples: List[Tuple[Path, Path]], transform: albu.Compose, stratified: bool = False) -> None:
         self.samples = samples
         self.transform = transform
+        self.stratified = stratified
+        self.grouped = self.group_samples()
 
     def __len__(self) -> int:
         return len(self.samples)
 
+    def group_samples(self):
+        result = {0: [], 1: []}
+        for image_path, target in self.samples:
+            result[target] += [image_path]
+        return result
+
     def __getitem__(self, idx: int) -> Dict[str, Any]:
+        # if self.stratified:
+        #     target = random.choice([0, 1])
+        #     image_path = random.choice(self.grouped[target])
+        # else:
         image_path, target = self.samples[idx]
 
         image = load_rgb(image_path, lib="jpeg4py")
 
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2YCR_CB)
+
         # apply augmentations
         image = self.transform(image=image)["image"]
 
-        return {"image_id": image_path.stem, "features": tensor_from_rgb_image(image), "targets": target}
+        return {
+            "image_id": image_path.stem,
+            "features": tensor_from_rgb_image(image),
+            "targets": torch.Tensor([target]),
+        }
 
 
 class AlaskaTest2Dataset(Dataset):
@@ -38,6 +59,8 @@ class AlaskaTest2Dataset(Dataset):
         image_path = self.samples[idx]
 
         image = load_rgb(image_path, lib="jpeg4py")
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2YCR_CB)
 
         # apply augmentations
         image = self.transform(image=image)["image"]
